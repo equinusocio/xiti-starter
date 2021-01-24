a6f42145ab2eb25cd5c105ce5fead2acc11b908a
Mattia <astorino.mattia@gmail.com>
2021-01-24 22:37:56 +0100

    chor: update comments

--- postcss.config.js
+++ postcss.config.js
@@ -1,10 +1,14 @@
 module.exports = {
   plugins: [
+    // https://preset-env.cssdb.org
     require('postcss-preset-env')({
       stage: 0,
     }),
+    // https://cssnano.co
     require('cssnano')({
       preset: [
+        // https://cssnano.co/docs/presets
+        // https://cssnano.co/docs/what-are-optimisations#what-optimisations-do-you-support
         'advanced',
         {
           discardComments: {
