ff13283346204959e95b7bd9ed94ac59487827f8
Mattia Astorino <equinusocio@users.noreply.github.com>
2021-03-16 16:32:23 +0100

    Update date.js

--- utils/filters/date.js
+++ utils/filters/date.js
@@ -1,29 +1,9 @@
-const appendSuffix = (n) => {
-  var s = ['th', 'st', 'nd', 'rd'],
-    v = n % 100
-  return n + (s[(v - 20) % 10] || s[v] || s[0])
-}
-
 module.exports = function (value) {
   const dateObject = new Date(value)
 
-  const months = [
-    'January',
-    'February',
-    'March',
-    'April',
-    'May',
-    'June',
-    'July',
-    'August',
-    'September',
-    'October',
-    'November',
-    'December',
-  ]
-  const dayWithSuffix = appendSuffix(dateObject.getDate())
-
-  return `${dayWithSuffix} ${
-    months[dateObject.getMonth()]
-  } ${dateObject.getFullYear()}`
+  return Intl.DateTimeFormat('en-US', {
+    month: "long",
+    day: "numeric",
+    year: "numeric"
+  }).format(dateObject)
 }
