d737283a28ed48586388b476678d564118bf2a1e
Mattia <astorino.mattia@gmail.com>
2021-04-16 15:27:18 +0200

    fix: theme switcher and remove turbolinks

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,29 +1,31 @@
-import Turbolinks from 'turbolinks'
-
 /**
  * Init Turbolinks within the site
  */
-Turbolinks.start()
 
 /**
  * Init ThemeSwitcher
  */
-const html = document.documentElement
 const themeSwitcher = document.getElementById('themeSwitcher')
-const currentTheme = localStorage.getItem('theme') || 'auto';
-themeSwitcher.value = currentTheme
 
-const setTheme = (theme) => {
-  html.dataset.theme = theme;
+/* Set the activated theme to root and save it to localStorage */
+function setTheme(theme) {
+  const root = document.documentElement
+
+  root.dataset.theme = theme
   localStorage.setItem('theme', theme)
 }
 
-themeSwitcher.addEventListener('change', () => {
-  if (themeSwitcher.value === 'auto') {
-    setTheme('auto')
-  } else if (themeSwitcher.value === 'light') {
-    setTheme('light')
-  } else if (themeSwitcher.value === 'dark') {
-    setTheme('dark')
+;(function () {
+  const root = document.documentElement
+  const currentTheme = localStorage.getItem('theme')
+
+  if (currentTheme) {
+    localStorage.setItem('theme', currentTheme)
+    themeSwitcher.value = currentTheme
+    root.dataset.theme = currentTheme
   }
-})
+
+  themeSwitcher.addEventListener('change', ({ target }) => {
+    setTheme(target.value)
+  })
+})()
